6a09100cdb76e466a470958c368c45beb08ea2d7
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-02-22 15:43:49 +0900

    add spring project

---
+++ src/main/java/com/flytrap/venusplanner/VenusPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.flytrap.venusplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flytrap/venusplanner/VenusPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flytrap.venusplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
