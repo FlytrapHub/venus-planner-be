5fe05ddd9f20578b7cb90f37b86637390d8a7acd
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-02-23 17:44:32 +0900

    feat: Plan 관련 Entity 추가

---
+++ src/main/java/com/flytrap/venusplanner/api/study/domain/PlanCategory.java
@@ -0,0 +1,36 @@
+package com.flytrap.venusplanner.api.study.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlanCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String fontColor;
+
+    @NotNull
+    private String backgroundColor;
+}
