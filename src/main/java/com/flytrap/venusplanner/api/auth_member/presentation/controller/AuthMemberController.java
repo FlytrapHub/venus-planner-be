b600fb2b1cfbfc5a169e5487de130c0a3e95dcc8
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-01 22:37:47 +0900

    refactor: Controller class 단위의 RequestMapping 어노테이션 제거
    - 검색의 편의를 위해 메서드 단위에 API URL을 입력하도록 한다

--- src/main/java/com/flytrap/venusplanner/api/auth_member/presentation/controller/AuthMemberController.java
+++ src/main/java/com/flytrap/venusplanner/api/auth_member/presentation/controller/AuthMemberController.java
@@ -1,8 +1,8 @@
 package com.flytrap.venusplanner.api.auth_member.presentation.controller;
 
 import com.flytrap.venusplanner.api.auth_member.business.service.AuthMemberService;
-import com.flytrap.venusplanner.api.member.domain.Member;
 import com.flytrap.venusplanner.api.auth_member.presentation.dto.LoginDto;
+import com.flytrap.venusplanner.api.member.domain.Member;
 import com.flytrap.venusplanner.global.auth.dto.SessionMember;
 import com.flytrap.venusplanner.global.auth.infrastructure.properties.AuthSessionProperties;
 import jakarta.servlet.http.HttpSession;
@@ -11,18 +11,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class AuthMemberController {
 
     private final AuthSessionProperties authSessionProperties;
     private final AuthMemberService authMemberService;
 
-    @PostMapping("/auth/sign-in")
+    @PostMapping("/api/v1/auth/sign-in")
     public ResponseEntity<LoginDto.Response> signIn(
             @RequestBody LoginDto.Request request,
             HttpSession session
@@ -33,7 +31,7 @@ public class AuthMemberController {
         return ResponseEntity.ok().body(LoginDto.Response.from(member));
     }
 
-    @DeleteMapping("/auth/sign-out")
+    @DeleteMapping("/api/v1/auth/sign-out")
     public ResponseEntity<Void> signOut(HttpSession session) {
 
         session.invalidate();
