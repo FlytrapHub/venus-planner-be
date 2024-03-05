6e34b146c90b2951ed35d647d2e4071160f4e6b6
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-05 14:23:36 +0900

    rename: GitHubOAuthRequestException 패키지 위치 global에서 auth_member로 변경

--- src/main/java/com/flytrap/venusplanner/api/auth_member/exception/GitHubOAuthRequestException.java
+++ src/main/java/com/flytrap/venusplanner/api/auth_member/exception/GitHubOAuthRequestException.java
@@ -1,4 +1,4 @@
-package com.flytrap.venusplanner.global.auth.exception;
+package com.flytrap.venusplanner.api.auth_member.exception;
 
 public class GitHubOAuthRequestException extends RuntimeException {
     public GitHubOAuthRequestException(String message) {
