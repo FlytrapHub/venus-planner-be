6246b8fb010d43a9e8f52cb788f09e3079ad103a
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-05 15:03:16 +0900

    fix: SessionMemberAuthException가 AuthenticationFailedException을 상속 받도록 변경

--- src/main/java/com/flytrap/venusplanner/global/auth/exception/SessionMemberAuthException.java
+++ src/main/java/com/flytrap/venusplanner/global/auth/exception/SessionMemberAuthException.java
@@ -1,6 +1,8 @@
 package com.flytrap.venusplanner.global.auth.exception;
 
-public class SessionMemberAuthException extends RuntimeException {
+import com.flytrap.venusplanner.global.exception.custom.AuthenticationFailedException;
+
+public class SessionMemberAuthException extends AuthenticationFailedException {
     public SessionMemberAuthException() {
         super("로그인이 필요한 기능입니다.");
     }
