e7dbeea7a03cd6da600316a5c9c9230c0acd2f26
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-13 16:04:04 +0900

    feat: Member 도메인에 TimeAuditingBaseEntity 적용

--- src/main/java/com/flytrap/venusplanner/api/member/domain/Member.java
+++ src/main/java/com/flytrap/venusplanner/api/member/domain/Member.java
@@ -1,25 +1,24 @@
 package com.flytrap.venusplanner.api.member.domain;
 
 import com.flytrap.venusplanner.api.auth_member.infrastructure.dto.StandardizedUserResource;
+import com.flytrap.venusplanner.global.entity.TimeAuditingBaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends TimeAuditingBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,9 +38,6 @@ public class Member {
 
     @NotNull
     private String nickname;
-
-    @CreatedDate
-    private Instant createdTime;
 
     @Builder
     private Member(String oauthPk, Long oauthPlatformId, String email, String profileImageUrl, String nickname) {
