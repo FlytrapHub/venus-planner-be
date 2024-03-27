ad6baca47c82ea209b822878e7d44ec20e1ce896
jaea-kim <jaeakim98@gmail.com>
2024-03-28 05:55:07 +0900

    refactor: 시간 변환 유틸함수 분리

--- src/main/java/com/flytrap/venusplanner/api/plan/domain/Frequency.java
+++ src/main/java/com/flytrap/venusplanner/api/plan/domain/Frequency.java
@@ -1,6 +1,7 @@
 package com.flytrap.venusplanner.api.plan.domain;
 
 import com.flytrap.venusplanner.api.plan.business.service.EndOptionCalculate;
+import com.flytrap.venusplanner.api.plan.util.DateTimeUtils;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -10,17 +11,15 @@ public enum Frequency implements EndOptionCalculate {
     WEEKLY {
         @Override
         public Instant calculateEndDate(Instant startDate, int count) {
-            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
-            ZonedDateTime zdtEnd = zdtStart.plusWeeks(count - 1);
-            return zdtEnd.toInstant();
+            ZonedDateTime zdtStart = DateTimeUtils.toZonedDateTime(startDate);
+            return DateTimeUtils.toInstant(zdtStart.plusWeeks(count - 1));
         }
 
         @Override
         public int calculateCount(Instant startDate, Instant endDate) {
-            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
-            ZonedDateTime zdtEnd = endDate.atZone(ZoneId.systemDefault());
-            long weeksBetween = ChronoUnit.WEEKS.between(zdtStart, zdtEnd);
-            return (int) weeksBetween + 1;
+            ZonedDateTime zdtStart = DateTimeUtils.toZonedDateTime(startDate);
+            ZonedDateTime zdtEnd = DateTimeUtils.toZonedDateTime(endDate);
+            return (int) ChronoUnit.WEEKS.between(zdtStart, zdtEnd) + 1;
         }
     },
     MONTHLY {
