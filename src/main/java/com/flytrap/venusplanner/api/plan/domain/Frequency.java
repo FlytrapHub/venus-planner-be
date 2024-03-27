76509f5f946a4810fd3c1404cab54287ad41693e
jaea-kim <jaeakim98@gmail.com>
2024-03-28 04:44:25 +0900

    feat: 연,월 연산하도록 시간 타입 변환 로직 추가

--- src/main/java/com/flytrap/venusplanner/api/plan/domain/Frequency.java
+++ src/main/java/com/flytrap/venusplanner/api/plan/domain/Frequency.java
@@ -2,6 +2,8 @@ package com.flytrap.venusplanner.api.plan.domain;
 
 import com.flytrap.venusplanner.api.plan.business.service.EndOptionCalculate;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 public enum Frequency implements EndOptionCalculate {
@@ -20,24 +22,32 @@ public enum Frequency implements EndOptionCalculate {
     MONTHLY {
         @Override
         public Instant calculateEndDate(Instant startDate, int count) {
-            return startDate.plus(count - 1, ChronoUnit.MONTHS);
+            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
+            ZonedDateTime zdtEnd = zdtStart.plusMonths(count - 1);
+            return zdtEnd.toInstant();
         }
 
         @Override
         public int calculateCount(Instant startDate, Instant endDate) {
-            long monthsBetween = ChronoUnit.MONTHS.between(startDate, endDate);
+            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
+            ZonedDateTime zdtEnd = endDate.atZone(ZoneId.systemDefault());
+            long monthsBetween = ChronoUnit.MONTHS.between(zdtStart, zdtEnd);
             return (int) monthsBetween + 1;
         }
     },
     YEARLY {
         @Override
         public Instant calculateEndDate(Instant startDate, int count) {
-            return startDate.plus(count - 1, ChronoUnit.YEARS);
+            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
+            ZonedDateTime zdtEnd = zdtStart.plusYears(count - 1);
+            return zdtEnd.toInstant();
         }
 
         @Override
         public int calculateCount(Instant startDate, Instant endDate) {
-            long yearsBetween = ChronoUnit.YEARS.between(startDate, endDate);
+            ZonedDateTime zdtStart = startDate.atZone(ZoneId.systemDefault());
+            ZonedDateTime zdtEnd = endDate.atZone(ZoneId.systemDefault());
+            long yearsBetween = ChronoUnit.YEARS.between(zdtStart, zdtEnd);
             return (int) yearsBetween + 1;
         }
     };
